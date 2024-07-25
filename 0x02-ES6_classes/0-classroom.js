8c536023f9bf9bd3854313c9bb14a9aaf29dbbe7
valritehachi <valritehachi@gmail.com>
2024-07-25 15:29:41 +0000

    edits

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,6 @@
+// Class representing a classroom with a maximum number of students
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
