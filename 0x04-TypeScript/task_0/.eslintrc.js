d81a0266cc409d8d047f65ca85102618d2b7b50b
valritehachi <valritehachi@gmail.com>
2024-07-31 14:27:18 +0000

    final comit

---
+++ 0x04-TypeScript/task_0/.eslintrc.js
@@ -0,0 +1,13 @@
+// a function that uses te recomended rules
+module.exports =  {
+  parser:  '@typescript-eslint/parser',
+  extends:  [
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parserOptions:  {
+    ecmaVersion:  2018,
+    sourceType:  'module',
+  },
+  rules:  {
+  },
+};
